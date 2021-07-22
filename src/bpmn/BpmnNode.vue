cc4ef4599f2b03442b95723a103e34b65f4135e7
zby <940150492@qq.com>
2021-07-22 14:34:34 +0800

    test

---
+++ src/bpmn/BpmnNode.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>1212</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/bpmn/BpmnProcess.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<a-collapse default-active-key="1" :bordered="false" expandIconPosition="right">
+			<a-collapse-panel key="1" header="基本设置">
+				<p>{{ text }}</p>
+			</a-collapse-panel>
+			<a-collapse-panel key="2" header="候选配置">
+				<p>{{ text }}</p>
+			</a-collapse-panel>
+			<a-collapse-panel key="3" header="监听器">
+				<p>{{ text }}</p>
+			</a-collapse-panel>
+			<a-collapse-panel key="4" header="附加属性">
+				<p>{{ text }}</p>
+			</a-collapse-panel>
+		</a-collapse>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text: `A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.`,
+			};
+		},
+	};
+</script>
+
+<style>
+</style>
